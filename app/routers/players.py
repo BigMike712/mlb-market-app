8d81ad22d4bef8fae3af2121cc0ace9041126eb6
Michael <mgamsel12@gmail.com>
2025-05-21 21:31:04 -0400

    Added a test route and made sure the FastAPI app is workign

--- app/routers/players.py
+++ app/routers/players.py
@@ -2,6 +2,11 @@ from fastapi import APIRouter
 
 router = APIRouter(prefix="/players", tags=["Players"])
 
+# When someone sends a GET request to the route /players/, call the function below:
 @router.get("/")
 def list_players():
     return {"message": "This will list player data soon!"}
+
+@router.get("/test")
+def say_hello():
+    return {"message": "Hello from your API!"}
